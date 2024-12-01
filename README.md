08828ce3ea2fa5a563e9dde760853ea3ce6b70c2
Lucas Tonolli <lucastonolli973@gmail.com>
2024-12-01 11:51:51 -0300

    Definição de ideia

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # clone-tabnews
 
 Projeto para aprimorar os conhecimentos de programação
+
+# 01/12/2024
+
+## otakunews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <h1>Olá!</h1>;
+	return (
+		<div>
+			<h1>Otakunews - Em Construção</h1>
+			<h2>Site de notícias de:</h2>
+			<ul>
+				<li>Animes</li>
+				<li>Mangás</li>
+				<li>Light Novels</li>
+				<li>Manhwa</li>
+				<li>Manhua</li>
+				<li>Webtoon</li>
+			</ul>
+		</div>
+	);
 }
 
 export default Home;
