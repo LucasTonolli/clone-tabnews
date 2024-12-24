1a99eb718969c1c405cded6af49dc7b938dd2029
Lucas Tonolli <lucastonolli973@gmail.com>
2024-12-24 10:03:28 -0300

    Makes database.js client.connect() more robust to errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,6 @@ async function status(request, response) {
 	const updatedAt = new Date().toISOString();
 
 	const databaseVersionResult = await database.query("SHOW server_version;");
-
 	const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
 	const databaseMaxConnectionsResult = await database.query(
@@ -18,7 +17,6 @@ async function status(request, response) {
 		text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
 		values: [databaseName],
 	});
-
 	const databaseOpenedConnectionsValue =
 		databaseOpenedConnectionsResult.rows[0].count;
 
