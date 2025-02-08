ce9b32a1e2156d1acda8cb461917ebf9a8019f3b
Lucas Tonolli <lucastonolli973@gmail.com>
2025-02-08 18:13:05 -0300

    correction to align with course

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,15 +6,19 @@ async function waitForAllServices() {
 	async function waitForWebServer() {
 		return retry(fetchStatusPage, {
 			retries: 100,
+			maxTimeout: 1000,
 		});
 
 		async function fetchStatusPage() {
 			const response = await fetch("http://localhost:3000/api/v1/status");
-			const body = await response.json();
+
+			if (response.status !== 200) {
+				throw Error();
+			}
 		}
 	}
 }
-
+//Comentário só para criar o pull request e fazer o amend
 export default {
 	waitForAllServices,
 };
