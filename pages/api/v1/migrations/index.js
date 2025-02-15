2161b4a3e896a3bb1f1ff78c6708ac67572f0601
Lucas Tonolli <lucastonolli973@gmail.com>
2025-02-15 16:07:32 -0300

    fix `prettier` style linting

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,39 +1,39 @@
 import migrationRunner from "node-pg-migrate";
-import {join} from "node:path";
+import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-	const method = request.method;
+  const method = request.method;
 
-	const allowedMethods = ["GET", "POST"];
+  const allowedMethods = ["GET", "POST"];
 
-	if (!allowedMethods.includes(method))
-		return response.status(405).json({
-			error: ` Method ${method} not allowed`,
-		});
+  if (!allowedMethods.includes(method))
+    return response.status(405).json({
+      error: ` Method ${method} not allowed`,
+    });
 
-	let dbClient;
+  let dbClient;
 
-	try {
-		dbClient = await database.getNewClient();
+  try {
+    dbClient = await database.getNewClient();
 
-		const migrationOptions = {
-			dbClient: dbClient,
-			databaseUrl: process.env.DATABASE_URL,
-			dir: join("infra", "migrations"),
-			direction: "up",
-			verbose: true,
-			dryRun: method == "GET",
-			migrationsTable: "pgmigrations",
-		};
-		const migrations = await migrationRunner(migrationOptions);
-		const statusCode = method == "POST" && migrations.length > 0 ? 201 : 200;
+    const migrationOptions = {
+      dbClient: dbClient,
+      databaseUrl: process.env.DATABASE_URL,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      dryRun: method == "GET",
+      migrationsTable: "pgmigrations",
+    };
+    const migrations = await migrationRunner(migrationOptions);
+    const statusCode = method == "POST" && migrations.length > 0 ? 201 : 200;
 
-		return response.status(statusCode).json(migrations);
-	} catch (error) {
-		console.error(error);
-		throw error;
-	} finally {
-		await dbClient.end();
-	}
+    return response.status(statusCode).json(migrations);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
