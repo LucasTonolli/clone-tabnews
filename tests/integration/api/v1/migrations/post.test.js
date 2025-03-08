fa87a1566ba716c1b85ed84edec856db4c816537
Lucas Tonolli <lucastonolli973@gmail.com>
2025-03-08 14:57:37 -0300

    style: fix with prettier

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,42 +1,41 @@
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
-	await orchestrator.waitForAllServices();
-	await orchestrator.clearDatabase();
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
 });
 
 describe("POST /api/v1/migrations", () => {
-	describe("Anonymous user", () => {
-		describe("Running pending migrations", () => {
-			const args = {
-				url: "http://localhost:3000/api/v1/migrations",
-				options: {
-					method: "POST",
-				},
-			};
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      const args = {
+        url: "http://localhost:3000/api/v1/migrations",
+        options: {
+          method: "POST",
+        },
+      };
 
-			test("For the first time", async () => {
-				const responsePending = await fetch(args.url, args.options);
-				expect(responsePending.status).toBe(201);
+      test("For the first time", async () => {
+        const responsePending = await fetch(args.url, args.options);
+        expect(responsePending.status).toBe(201);
 
-				const responseBody = await responsePending.json();
+        const responseBody = await responsePending.json();
 
-				expect(Array.isArray(responseBody)).toBe(true);
-				expect(responseBody.length).toBeGreaterThan(0);
-			});
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
 
-			test("For the second time", async () => {
-				const responseWithoutMigrations = await fetch(args.url, args.options);
+      test("For the second time", async () => {
+        const responseWithoutMigrations = await fetch(args.url, args.options);
 
-				expect(responseWithoutMigrations.status).toBe(200);
+        expect(responseWithoutMigrations.status).toBe(200);
 
-				const responseWithoutMigrationsBody =
-					await responseWithoutMigrations.json();
-				expect(Array.isArray(responseWithoutMigrationsBody)).toBe(true);
+        const responseWithoutMigrationsBody =
+          await responseWithoutMigrations.json();
+        expect(Array.isArray(responseWithoutMigrationsBody)).toBe(true);
 
-				expect(responseWithoutMigrationsBody.length).toBe(0);
-			});
-		});
-	});
+        expect(responseWithoutMigrationsBody.length).toBe(0);
+      });
+    });
+  });
 });
-
