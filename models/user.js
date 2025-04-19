b2090b3d89a77bbc925250bd36fc6055261b917e
Lucas Tonolli <lucastonolli973@gmail.com>
2025-04-19 14:11:36 -0300

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 import database from "infra/database";
 import { ValidationError, NotFoundError } from "infra/errors";
+import password from "models/password.js";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -49,6 +51,11 @@ async function create(userInputValues) {
         action: "Utilize outro username para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashed = await password.hash(userInputValues.password);
+    userInputValues.password = hashed;
   }
 
   async function runInsertQuery(userInputValues) {
